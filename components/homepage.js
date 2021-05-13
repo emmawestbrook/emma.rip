06d41470ba02af80b90cf096b7a70e4a07a093a7
Emma Westbrook <emmalwestbrook@gmail.com>
2021-05-12 21:45:20 -0500

    initial commit

---
+++ components/homepage.js
@@ -0,0 +1,18 @@
+import { CMS_NAME } from '../lib/constants'
+
+export default function Homepage() {
+    return (
+        <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+
+            <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+                some of{' '}
+                <a
+                    href="/blog"
+                    className="underline hover:text-success font-mono duration-200 transition-colors"
+                >
+                    my thoughts
+        </a>{' '}
+            </h4>
+        </section>
+    )
+}
---
+++ components/intro.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+export default function Intro() {
+  return (
+    <section className="flex-col md:flex-row flex items-center md:justify-between md:mx-10 mt-16 mb-16 md:mb-12">
+      <h1 className="text-5xl md:text-7xl font-tactile tracking-tighter leading-tight md:pr-8">
+        emma
+        .
+        <a
+          href="/blog"
+          className="text-3xl md:text-5xl underline hover:text-cyan font-mono duration-200 transition-colors"
+        >rip
+        </a>{' '}
+      </h1>
+      <a
+        href="/"
+        className=""
+      >
+        <Image className="" src='/sun.png' width='100px' height='100px' />
+
+      </a>
+
+    </section>
+  )
+}
