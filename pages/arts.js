4ab82042288cc19e8961d77027fcf422ec916ef3
Emma Westbrook <emmalwestbrook@gmail.com>
2021-05-18 18:52:32 -0500

    upload artwork files

--- pages/arts.js
+++ pages/arts.js
@@ -4,11 +4,11 @@ import Artwork from '../components/artwork'
 export default function Arts() {
     return (
         <Layout>
-            <section className="text-center md:text-left py-16">
+            <section className="text-center md:text-left">
                 <h1 className="text-5xl md:text-7xl font-tactile ">
                     this page is coming soon.
              </h1>
-                <div className="grid grid-cols-1 lg:grid-cols-2 lg:mx-8 lg:gap-x-8 gap-y-16 mb-32 ">
+                <div className="grid grid-cols-1 lg:grid-cols-2 lg:mx-8 lg:gap-x-8 gap-y-4 mb-32 ">
 
                     <Artwork />
                     <Artwork />
