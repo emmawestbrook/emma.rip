0ab5ff36d894aa4a679a602b5ea08ced72e40e60
Emma Westbrook <emmalwestbrook@gmail.com>
2021-05-18 21:53:05 -0500

    hoping styles work now

--- pages/arts.js
+++ pages/arts.js
@@ -54,19 +54,19 @@ export default function Arts() {
     ];
     return (
         <Layout>
-            <section className="text-center md:text-left">
-                <h1 className="text-5xl md:text-7xl font-tactile ">
+            <section className="">
+                <h1 className="text-5xl md:text-7xl font-tactile mb-16 text-center md:text-left">
                     some of my art
              </h1>
                 <div className="grid grid-cols-1 lg:mx-8 lg:gap-x-8 mb-32 ">
                     {artwork.map((art) => (
-                        <div className=" px-4 lg:py-4  lg:flex flex-row">
+                        <div className=" px-4 lg:py-4  lg:flex lg:flex-row">
                             <div className="">
                                 <img className=" pt-8 lg:py-8 max-w-100 xl:max-w-screen-sm max-h-600" src={'/assets/artwork/' + art.image} />
 
                             </div>
-                            <div className="mb-8 lg:pt-24">
-                                <div className=" px-6 py-6 border border-orange border-dashed">
+                            <div className="mb-8 lg:pt-24 text-center lg:text-left">
+                                <div className=" px-6 py-6 border border-orange border-dashed ">
                                     <h3 className="text-2xl font-mono text-blue">{art.title}</h3>
                                     <div className="text-lg pt-4 font-tactile">{art.medium}</div>
                                     <div className="text-xl font-tactile">{art.date}</div>
